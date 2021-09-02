6378feb89fe79c986ae14653fee4a833ee312381
DONALDBZR <andygaspard@hotmail.com>
2021-09-02 21:22:20 +0400

    React.js Implementation on the Homepage

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # StormySystems2
+
 Web-Based Multi-Systems Application
+
 # COMMIT 1
+
 Creation of the file which has all the servers configurations
+
+# COMMIT 2
+
+Homepage creation with React.JS
---
+++ Scripts/Homepage.js
@@ -0,0 +1,64 @@
+// Header Class
+class Header extends React.Component {
+    // Render method
+    render() {
+        return (
+            <header>
+                <nav>
+                    <div>
+                        <a href="./">
+                            <img src="./Images/Logo.png" alt="System Logo" />
+                        </a>
+                    </div>
+                    <div>
+                        <a href="./AboutUs">About Us</a>
+                    </div>
+                    <div>
+                        <a href="./Service">Services Offered</a>
+                    </div>
+                    <div>
+                        <a href="./Projects">Projects</a>
+                    </div>
+                    <div>
+                        <a href="./Login">Login</a>
+                    </div>
+                    <div>
+                        <a href="./Register">Register</a>
+                    </div>
+                </nav>
+            </header>
+        );
+    }
+}
+// Main Class
+class Main extends React.Component {
+    // Render method
+    render() {
+        return (
+            <main>
+                <h1>Welcome to</h1>
+                <img src="./Images/Logo.png" alt="System Logo" />
+            </main>
+        );
+    }
+}
+// Footer Class
+class Footer extends React.Component {
+    // Render method
+    render() {
+        return (
+            <footer>
+                <h1>Stormy Systems</h1>
+            </footer>
+        );
+    }
+}
+// Homepage class
+class Homepage extends React.Component {
+    // Render method
+    render() {
+        return [<Header />, <Main />, <Footer />];
+    }
+}
+// Rendering ./
+ReactDOM.render(<Homepage />, document.getElementById("app"));
