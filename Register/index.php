eb9077a8f615ca7b6688d0e877f638dfe52aee17
DONALDBZR <andygaspard@hotmail.com>
2021-10-14 18:56:30 +0400

    Update 2.39 - Debugging

--- Register/index.php
+++ Register/index.php
@@ -34,13 +34,139 @@ ob_start();
         <script src="../Scripts/StormySystems.js"></script>
     </head>
     <body id="app">
-        <script type="text/babel" src="../Scripts/Register.js"></script>
-        <?php
-        // If-statement to verify whether the registration button is pressed
-        if (isset($_POST["register"])) {
-            $User->register();
-        }
-        ?>
+        <!-- <script type="text/babel" src="../Scripts/Register.js"></script> -->
+        <header>
+            <nav>
+                <div>
+                    <a href="../">
+                        <img src="../Images/Logo.png" alt="System Logo" />
+                    </a>
+                </div>
+                <div>
+                    <a href="../AboutUs">About Us</a>
+                </div>
+                <div>
+                    <a href="../Service">Services Offered</a>
+                </div>
+                <div>
+                    <a href="../Projects">Projects</a>
+                </div>
+                <div>
+                    <a href="../Login">Login</a>
+                </div>
+                <div>
+                    <a href="./">Register</a>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <form method="post" action="./">
+                <div id="label">Registration Form</div>
+                <div id="formContainer">
+                    <div id="username">
+                        <div id="input">
+                            <div>Username *:</div>
+                            <input
+                                type="text"
+                                name="username"
+                                id="username"
+                                placeholder="Username"
+                                required
+                            />
+                        </div>
+                        <div id="guidelines">
+                            Please enter a username which is unique!
+                        </div>
+                        <div id="guidelines">
+                            Ensure that your username is not NSFW!
+                        </div>
+                    </div>
+                    <div id="mailAddress">
+                        <div id="input">
+                            <div>Mail Address *:</div>
+                            <input
+                                type="email"
+                                name="mailAddress"
+                                id="mailAddress"
+                                placeholder="Mail Address"
+                                required
+                            />
+                        </div>
+                        <div id="guidelines">
+                            The Mail Address is required to send you your
+                            password and to access your account, afterwards.
+                        </div>
+                    </div>
+                    <div id="firstName">
+                        <div id="input">
+                            <div>First Name *:</div>
+                            <input
+                                type="text"
+                                name="firstName"
+                                id="firstName"
+                                placeholder="First Name"
+                                required
+                            />
+                        </div>
+                        <div id="guidelines">
+                            Please enter your first name!
+                        </div>
+                    </div>
+                    <div id="lastName">
+                        <div id="input">
+                            <div>Last Name *:</div>
+                            <input
+                                type="text"
+                                name="lastName"
+                                id="lastName"
+                                placeholder="Last Name"
+                                required
+                            />
+                        </div>
+                        <div id="guidelines">
+                            Please enter your last name!
+                        </div>
+                    </div>
+                    <div id="dateOfBirth">
+                        <div id="input">
+                            <div>Date Of Birth *:</div>
+                            <input
+                                type="date"
+                                name="dateOfBirth"
+                                id="dateOfBirth"
+                                required
+                            />
+                        </div>
+                        <div id="guidelines">
+                            Please enter your date of birth!
+                        </div>
+                        <div id="guidelines">
+                            You need to be at least thirteen years old to
+                            register into this system!
+                        </div>
+                    </div>
+                    <div id="registrationButton">
+                        <input
+                            type="submit"
+                            value="Register"
+                            name="register"
+                            onClick={User.register()}
+                        />
+                    </div>
+                </div>
+                <div id="serverRendering">
+                    <?php
+                    // If-statement to verify whether the registration button is pressed
+                    if (isset($_POST["register"])) {
+                        $User->register();
+                    }
+                    ?>
+                </div>
+            </form>
+        </main>
+        <footer>
+            <h1>Stormy Systems</h1>
+        </footer>
     </body>
 </html>
 <?php
