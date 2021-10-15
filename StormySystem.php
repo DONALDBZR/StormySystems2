ad8c02eea13598a48c91c3e87ceb4f3ad385cdac
DONALDBZR <andygaspard@hotmail.com>
2021-10-15 18:45:28 +0400

    Update 2.52 - Debugging

--- StormySystem.php
+++ StormySystem.php
@@ -190,7 +190,7 @@ class User {
     // Register method
     public function register() {
         // Receiving the JSON from the POST Request
-        $userJSON = utf8_encode(file_get_contents('php://input'));
+        $userJSON = mb_convert_encoding(file_get_contents('php://input'), "UTF-8");
         // Decoding User JSON into a PHP Object
         $userObject = json_decode($userJSON, true);
         // Printing the Object
