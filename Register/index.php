852229774067f48cad6ee5c18223725bd35c2caa
DONALDBZR <andygaspard@hotmail.com>
2021-10-17 00:26:30 +0400

    Update 2.184 - Debugging

--- Register/index.php
+++ Register/index.php
@@ -3,7 +3,7 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/StormySystems2/StormySystem.php';
 // Instantiation User
 $User = new User();
-// If-Statement to verify the type of the request sent
+// If-Statement to verify whether there is a JSON
 if (json_decode(file_get_contents("php://input")) != null) {
     // Calling User::register()
     $User->register();
