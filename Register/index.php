2276d83bbd3b2b059494ff986408e5c45f2a967b
DONALDBZR <andygaspard@hotmail.com>
2021-10-19 18:48:13 +0400

    Update 2.289 - Debugging

--- Register/index.php
+++ Register/index.php
@@ -5,8 +5,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/StormySystems2/Pages/Register.html';
 ob_start();
 // Storing the contents of the output buffer into a variable
 $html = ob_get_contents();
-// Deleting the contents of the output buffer.
-ob_end_flush();
 // Printing the html page
 echo $html;
+// Deleting the contents of the output buffer.
+ob_end_flush();
 ?>
