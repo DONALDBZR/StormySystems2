6ba04159b8c97ad4760c68db597973255bb66672
DONALDBZR <andygaspard@hotmail.com>
2021-10-19 18:19:33 +0400

    Update 2.280 - Debugging

--- Register/index.php
+++ Register/index.php
@@ -1,3 +1,12 @@
 <?php
+// Importing Register.html
 require_once $_SERVER['DOCUMENT_ROOT'] . '/StormySystems2/Pages/Register.html';
+// Starting Output Buffer
+ob_start();
+// Storing the contents of the output buffer into a variable
+$html = ob_get_contents();
+// Deleting the contents of the output buffer.
+ob_end_clean();
+// Printing the html page
+echo $html;
 ?>
