3bceadd5a6003f22d6326cf1243f3f6254c1991f
DONALDBZR <andygaspard@hotmail.com>
2021-10-19 19:56:08 +0400

    Update 2.300 - Debugging

--- Scripts/Register.js
+++ Scripts/Register.js
@@ -87,14 +87,10 @@ class Main extends Register {
                     message: data.message,
                     url: data.url,
                 })
-            );
-        // Generating another POST request to redirect after rendering the page by using fetch()
-        fetch(this.state.url, {
-            method: "POST",
-            redirect: "follow",
-        }).then(() => {
-            window.location.href = this.state.url;
-        });
+            )
+            .then(() => {
+                window.location.href = this.state.url;
+            });
     }
     // Render method
     render() {
