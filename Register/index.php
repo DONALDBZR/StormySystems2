c827e1ac563f3967668cef6323820fe5c1fb022f
DONALDBZR <andygaspard@hotmail.com>
2021-10-19 09:15:54 +0400

    Update 2.224 - Debugging

--- Register/index.php
+++ Register/index.php
@@ -17,15 +17,8 @@
     <body id="app">
         <script async type="text/babel" src="../Scripts/Register.js"></script>
         <?php
-        // Importing User.php
-        require_once $_SERVER['DOCUMENT_ROOT'] . '/StormySystems2/StormySystem.php';
-        // Instantiation User
-        $User = new User();
-        // If-Statement to verify whether there is a JSON
-        if (json_decode(file_get_contents("php://input")) != null) {
-            // Calling User::register()
-            $User->register();
-        }
+        // Importing Register.php
+        require "./Register.php";
         ?>
     </body>
 </html>
