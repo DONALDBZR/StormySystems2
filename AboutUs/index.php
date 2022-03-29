41da2d91636937969cd3e2f5522c0188a3daf7f2
DONALDBZR <andygaspard@hotmail.com>
2022-03-29 17:26:34 +0400

    Update 2.6
    REWORK:  Using Node.JS instead of PHP

--- AboutUs/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/StormySystems2/public/Pages/AboutUs.html';
-?>
--- Register/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Importing Register.html
-require_once $_SERVER['DOCUMENT_ROOT'] . '/StormySystems2/public/Pages/Register.html';
-?>
